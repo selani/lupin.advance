e41d545d24bc638c15394a364e3fab174313924d
Administrator <selani@naver.com>
2025-04-19 22:06:39 +0900

    init

---
+++ src/CharsetTest.java
@@ -0,0 +1,41 @@
+import org.apache.commons.lang3.StringUtils;
+import org.junit.Test;
+
+import java.nio.charset.Charset;
+
+/**
+ * KSC5601  = KSC5601-1987
+ * EUCKR    = KSC5601-1987 + KSC5636(한글이외 문자표시규약)
+ * CP949    = KSC5601-1997 + 확장한글
+ * MS949    = KSC5601-1992 + 확장한글
+ * UTF8     = 전세계 데이터 송수신 권장 문자셋, 최대 6 byte, 한글 3 byte, 영문 1 byte
+ * AL32UTF8 = Oracle 9i 이상 기본 및 권장 문자셋, 최대 4 byte, 한글 3 byte, 영문 1 byte
+ */
+public class CharsetTest {
+
+    public static String pad(String s) {
+        int x = 0;
+        for (char c : s.toCharArray()) if (c < 'ㄱ') x++;
+        return s.concat(StringUtils.repeat(' ', x));
+    }
+
+    @Test
+    public void t1() throws Exception {
+        String[] sa = {"오렌즤", "오렌즹", "똠뷱뾸"};
+        String[] ca = {"KSC5601", "EUCKR", "CP949", "MS949", "UTF8", "KSC5601-1992"};
+        for (String s : sa) {
+            for (String c : ca) {
+                byte[] ba = s.getBytes(c);
+                System.out.printf("%s -> %s : %-7s (%2d) [ ", s, pad(new String(ba, c)), c, ba.length);
+                for (byte b : ba) System.out.printf("%02x ", b);
+                System.out.printf("] %n");
+            }
+            System.out.println();
+        }
+    }
+
+    @Test
+    public void t2() {
+        Charset.availableCharsets().values().forEach(System.out::println);
+    }
+}
